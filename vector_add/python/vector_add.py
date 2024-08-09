508afd56df2cf217a7c36de9e2e363842b4337e5
Mark Dewing <markdewing@gmail.com>
2024-08-09 10:48:43 -0500

    Print additional information and test output

--- vector_add/python/vector_add.py
+++ vector_add/python/vector_add.py
@@ -1,17 +1,24 @@
 import numpy as np
 
-def vector_add(a,b,c):
+
+def vector_add(a, b, c):
     # Reference storage of c versus assigning a new temporary to c
     c[:] = a + b
 
 
 n = 100
-a = np.ones(n,dtype=np.float32)
-b = np.linspace(start=0.0, stop=1.0, num=n, dtype=np.float32)
+a = np.ones(n, dtype=np.float32)
+b = np.linspace(start=0.0, stop=1.0 * (n - 1), num=n, dtype=np.float32)
+c_expect = np.linspace(start=1.0, stop=1.0 * n, num=n, dtype=np.float32)
 
 c = np.zeros_like(a)
-#print('b',b)
-#print('a',a)
+# print('b',b)
+# print('a',a)
 
-vector_add(a,b,c)
-print('c',c[0:10])
+print(f"Running vector add with vector size {n}")
+vector_add(a, b, c)
+print("c", c[0:10])
+
+np.testing.assert_allclose(c, c_expect)
+
+print("Pass!")
