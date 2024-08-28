945f3fc6f5ca9634546aa2f635c632d9465a5aab
Mark Dewing <markdewing@gmail.com>
2024-08-28 15:20:21 -0500

    Improve synchronization
    Perform synchronization after each function call in the calibration
    loop.
    
    Perform synchronization on the final function call in the benchmark
    loop.

--- vector_add/jax/timed_vector_add.py
+++ vector_add/jax/timed_vector_add.py
@@ -21,6 +21,12 @@ def vector_add(x, y):
     return jnp.add(x, y)
 
 
+def vector_add_sync(x, y):
+    c = vector_add(x, y)
+    c.block_until_ready()
+    return c
+
+
 print("# Jax version: ", jax.__version__)
 print("#", "CPU" if use_cpu else "GPU")
 print(
@@ -28,7 +34,7 @@ print(
 )
 
 
-pts = numpy.logspace(2, 9, num=20, dtype=numpy.int64)
+pts = numpy.logspace(2, 8.4, num=60, dtype=numpy.int64)
 for n in pts:
     a = jnp.ones(n, dtype=jnp.float32)
     b = jnp.linspace(start=0.0, stop=1.0 * (n - 1), num=n, dtype=jnp.float32)
@@ -37,7 +43,7 @@ for n in pts:
     # Warmup
     c = vector_add(a, b)
 
-    timer = timeit.Timer(lambda: vector_add(a, b))
+    timer = timeit.Timer(lambda: vector_add_sync(a, b))
 
     loops_per_cutoff, elapsed_calibration = timer.autorange()
     # Compute number of loops to run for about one second
@@ -46,7 +52,7 @@ for n in pts:
     start = timeit.default_timer()
     for it in range(nloop):
         c = vector_add(a, b)
-        c.block_until_ready()
+    c.block_until_ready()
     end = timeit.default_timer()
     elapsed = end - start
     # print("c", c[0:10])
@@ -57,7 +63,8 @@ for n in pts:
     bw = 3 * nbytes / elapsed_per_loop  # 2 reads and 1 write
 
     print(
-        f"{n:10} {nloop:10} {3*nbytes/1e6:12.4g} {elapsed:16.3f} {elapsed_per_loop*1e6:16.4g} {bw/1e9:10.3f}"
+        f"{n:10} {nloop:10} {3*nbytes/1e6:12.4g} {elapsed:16.3f} {elapsed_per_loop*1e6:16.4g} {bw/1e9:10.3f}",
+        flush=True,
     )
 
 
